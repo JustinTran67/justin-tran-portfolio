3c6e806a11db4948bd67ac48713c8e71b16e243b
Justin Tran <justintran@Justins-MacBook-Pro.local>
2025-12-03 03:01:15 -0800

    tech setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className="flex items-center justify-center text-center">
+      <h1 className="font-bold text-[64px]">Justin Tran</h1>
+    </div>
+  )
+}
+
+export default App
